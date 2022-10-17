cbbd2cbe1d955297b4a03fcf3a5195bef1abf21f
JuwonCha <juwon1512@gmail.com>
2022-10-17 23:35:53 +0900

    Add HUD UI

--- Source/ProjectTPS/public/ShooterPlayerController.h
+++ Source/ProjectTPS/public/ShooterPlayerController.h
@@ -17,7 +17,13 @@ class PROJECTTPS_API AShooterPlayerController : public APlayerController
 public:
 	virtual void GameHasEnded(class AActor* EndGameFocus = nullptr, bool bIsWinner = false) override;
 
+protected:
+	virtual void BeginPlay() override;
+
 private:
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class UUserWidget> HUDClass;
+
 	UPROPERTY(EditAnywhere)
 	TSubclassOf<class UUserWidget> WinScreenClass;
 
@@ -26,6 +32,9 @@ private:
 
 	UPROPERTY(EditAnywhere)
 	float restartDelay = 5.0f;
+
+	UPROPERTY()
+	UUserWidget* HUD;
 
 	FTimerHandle restartTimer;
 };
